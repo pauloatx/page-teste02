4e475c0116ff91463f287163f79807e9154d5b3e
pauloatx <paulo.artur999x@gmail.com>
2024-12-28 22:03:12 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirServicos() {
+    alert('Programação e Designer de sites')
+}
+function abrirContatos() {
+    alert('E-Mail: Paulo.999x@atx.com | Telefone: (83) 99418-4451')
+}
+function abrirRedes() {
+    alert('Instagram: @ATXsolutions')
+}
