424290f00025f3461fe38b9733ac5770b4be4d43
pauloatx <paulo.artur999x@gmail.com>
2025-02-01 23:25:50 -0300

    update efeito sujir

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function abrirServicos() {
-    alert('Programação e Designer de sites')
-}
-function abrirContatos() {
-    alert('E-Mail: Paulo.999x@atx.com | Telefone: (83) 99418-4451')
-}
-function abrirRedes() {
-    alert('Instagram: @ATXsolutions')
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const elementos = document.querySelectorAll(".contweb, .contmobile, .contdados");
+
+    const observer = new IntersectionObserver((entradas, observer) => {
+        entradas.forEach(entrada => {
+            if (entrada.isIntersecting) {
+                entrada.target.classList.add("apareceu"); // Adiciona a classe quando o elemento estiver visível
+                observer.unobserve(entrada.target); // Para de observar o elemento após ele aparecer
+            }
+        });
+    }, {
+        threshold: 0.5  // O elemento precisa estar 50% visível
+    });
+
+    elementos.forEach(elemento => {
+        observer.observe(elemento); // Observa os elementos
+    });
+});
